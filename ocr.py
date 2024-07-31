edf1623651d83bbfecd39dc5c523e947208223cc
Danny Liang <dannyliang2005@gmai.com>
2024-07-31 10:08:06 -0400

    update readme

--- ocr.py
+++ ocr.py
@@ -16,7 +16,7 @@ def extract_text(image_path):
         return {'OD': {'POWER': 'Error', 'BC': 'Error', 'DIA': 'Error'},
                 'OS': {'POWER': 'Error', 'BC': 'Error', 'DIA': 'Error'}}, "Error loading image"
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Convert image to graystone using OpenCV
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Convert image to grayscale using OpenCV
     text = pytesseract.image_to_string(gray) # Extracts text using pytesseract
 
     pattern = re.compile(r'(OD|OS)\s*([\d\.\-]+)\s*([\d\.\-]+)\s*([\d\.\-]+)', re.IGNORECASE) # Compile a regex pattern to match OD and OS measurements
